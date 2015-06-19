c7fc06e741d6af514d5d284de16b8b857229297d
ILYA Khlopotov <iilyak@ca.ibm.com>
2015-06-19 10:47:05 -0700

    Add couch_db_plugin:after_doc_read/2

--- src/couch_db_plugin.erl
+++ src/couch_db_plugin.erl
@@ -2,7 +2,8 @@
 
 -export([
     validate_dbname/2,
-    before_doc_update/2
+    before_doc_update/2,
+    after_doc_read/2
 ]).
 
 -include_lib("couch/include/couch_db.hrl").
@@ -18,6 +19,12 @@ validate_dbname(DbName, Normalized) ->
 
 before_doc_update(#db{before_doc_update = Fun} = Db, Doc0) ->
     case with_pipe(before_doc_update, [Doc0, Db]) of
+        [Doc1, _Db] when is_function(Fun) -> Fun(Doc1, Db);
+        [Doc1, _Db] -> Doc1
+    end.
+
+after_doc_read(#db{after_doc_read = Fun} = Db, Doc0) ->
+    case with_pipe(after_doc_read, [Doc0, Db]) of
         [Doc1, _Db] when is_function(Fun) -> Fun(Doc1, Db);
         [Doc1, _Db] -> Doc1
     end.
