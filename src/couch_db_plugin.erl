ee5a647f2833b5b8c57cec35f480a9bde8fade04
ILYA Khlopotov <iilyak@ca.ibm.com>
2015-06-19 10:41:32 -0700

    Add couch_db_plugin:before_doc_update/2

--- src/couch_db_plugin.erl
+++ src/couch_db_plugin.erl
@@ -1,6 +1,9 @@
 -module(couch_db_plugin).
 
--export([validate_dbname/2]).
+-export([
+    validate_dbname/2,
+    before_doc_update/2
+]).
 
 -include_lib("couch/include/couch_db.hrl").
 
@@ -13,7 +16,19 @@ validate_dbname(DbName, Normalized) ->
     %% callbacks return true only if it specifically allow the given Id
     couch_epi:any(Handle, couch_db, validate_dbname, [DbName, Normalized], [ignore_providers]).
 
+before_doc_update(#db{before_doc_update = Fun} = Db, Doc0) ->
+    case with_pipe(before_doc_update, [Doc0, Db]) of
+        [Doc1, _Db] when is_function(Fun) -> Fun(Doc1, Db);
+        [Doc1, _Db] -> Doc1
+    end.
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
+
+with_pipe(Func, Args) ->
+    do_apply(Func, Args, [ignore_providers, pipe]).
+
+do_apply(Func, Args, Opts) ->
+    Handle = couch_epi:get_handle(couch_db),
+    couch_epi:apply(Handle, couch_db, Func, Args, Opts).
